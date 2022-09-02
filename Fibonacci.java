c6fe5ea4a4b66e9fa4c906d42caaa0db8fef0357
KelleeR <47515247+KelleeR@users.noreply.github.com>
2022-09-01 21:17:20 -0400

    added javadocs

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,6 +2,13 @@ package assignment2;
 
 public class Fibonacci {
 
+	/**
+	*The main method calls the ficonacci sequence
+	*@param n is the position of the number
+	*@return the number resulting from the sequence
+	*@author KMRoberts
+	*/
+
 	static int fib(int n)
     {
     if (n <= 1)
